87232d81e8d5105f97aa85d7066ff65058922f34
Andrew Christie <awchristie86@gmail.com>
2021-11-18 22:24:41 -0500

    spinning my wheels

---
+++ graph/resolver.go
@@ -0,0 +1,53 @@
+package graph
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/achristie/gql-sample/graph/model"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CharacterStore map[string]model.Character
+	OutageService  OutageService
+}
+
+type HTTPClient interface {
+	Fetch(req *http.Request) (*http.Response, error)
+}
+
+type OutageService struct {
+	baseUrl    *url.URL
+	httpClient *http.Client
+}
+
+func NewOutageService() (*OutageService, error) {
+	u, _ := url.Parse("https://api.platts.com/refinery-data/v1/outage-alerts")
+	o := &OutageService{
+		baseUrl:    u,
+		httpClient: http.DefaultClient,
+	}
+
+	return o, nil
+}
+
+// outage.get(id=id, )
+
+func (o *OutageService) Get(req *http.Request) (*http.Response, error) {
+	log.Printf("[%s] %s", req.Method, req.URL)
+
+	resp, err := o.httpClient.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	return resp, nil
+}
